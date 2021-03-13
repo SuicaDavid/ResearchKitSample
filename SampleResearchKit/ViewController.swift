fec5ca871f6f2ea54bb0b9a56acee48c3538138f
SuicaDavid <genglanghuang@gmail.com>
2021-03-13 01:27:47 +0000

    create project

--- SampleResearchKit/ViewController.swift
+++ SampleResearchKit/ViewController.swift
@@ -6,14 +6,38 @@
 //
 
 import UIKit
+import ResearchKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ORKTaskViewControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+     
+    @IBAction func consentClicked(sender : AnyObject) {
+        print(1)
+        let taskViewController = ORKTaskViewController(task: ConsentTask, taskRun: nil)
+        taskViewController.delegate = self
+        present(taskViewController, animated: true, completion: nil)
+    }
+        
+    @IBAction func surveyClicked(sender : AnyObject) {
+        let taskViewController = ORKTaskViewController(task: SurveyTask, taskRun: nil)
+        taskViewController.delegate = self
+        present(taskViewController, animated: true, completion: nil)
+    }
+        
+    @IBAction func activeTaskClicked(sender : AnyObject) {
+    }
+    
+    func taskViewController(_ taskViewController: ORKTaskViewController, didFinishWith reason: ORKTaskViewControllerFinishReason, error: Error?) {
+            taskViewController.dismiss(animated: true, completion: nil)
+    }
 }
 
